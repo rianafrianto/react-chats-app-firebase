80333180e4e5beec3fed381dc697765b43489816
rianafrianto <rianafrianto17@gmail.com>
2024-05-12 18:47:43 +0700

    add new background

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+const App = () => {
+
+  return (
+    <>
+      <div className='container'>
+       Hello World, Rian
+      </div>
+    </>
+  )
+}
+
+export default App
